a39573270206f1230b35baf8315d6aee26543a4d
yu.ma <my_joy1@qq.com>
2023-12-01 15:35:45 +0800

    Initial Commit

---
+++ AbstractDemo/AbstractDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbstractDemoApp.swift
+//  AbstractDemo
+//
+//  Created by Yu Ma on 2023/12/1.
+//
+
+import SwiftUI
+
+@main
+struct AbstractDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
